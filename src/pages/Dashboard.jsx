1b02e7ed5601433101cb6ec25aca3421da8d099a
saiksaif <116622210+saiksaif@users.noreply.github.com>
2023-11-03 16:20:13 +0500

    Changed stats icons

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,66 +5,68 @@ function Dashboard() {
     {
       title: "Wallet",
       stat: 12,
-      image: "/svgIcons/wallet.svg"
+      image: "/svgIcons/wallet2.svg"
     },
     {
       title: "Notifications",
       stat: 3,
-      image: "/svgIcons/notification.svg"
+      image: "/svgIcons/notification2.svg"
     },
     {
       title: "Messages",
       stat: 4,
-      image: "/svgIcons/message.svg"
+      image: "/svgIcons/message2.svg"
     },
     {
       title: "Subscribers",
       stat: 240,
-      image: "/svgIcons/subscriber.svg"
+      image: "/svgIcons/subscriber2.svg"
     },
     {
       title: "Administrators",
       stat: 5,
-      image: "/svgIcons/administrator.svg"
+      image: "/svgIcons/administrator2.svg"
     },
     {
       title: "Admins",
       stat: 2,
-      image: "/svgIcons/administrator.svg"
+      image: "/svgIcons/administrator2.svg"
     },
     {
       title: "Agencies",
       stat: 4,
-      image: "/svgIcons/agency.svg"
+      image: "/svgIcons/agency2.svg"
     },
     {
       title: "Hosts",
       stat: 12,
-      image: "/svgIcons/host.svg"
+      image: "/svgIcons/host2.svg"
     },
     {
       title: "Agents",
       stat: 14,
-      image: "/svgIcons/agent.svg"
+      image: "/svgIcons/agent2.svg"
     },
     {
       title: "Users",
       stat: 540,
-      image: "/svgIcons/users.svg"
+      image: "/svgIcons/users2.svg"
     },
     {
       title: "Blocked",
       stat: 21,
-      image: "/svgIcons/blocked.svg"
+      image: "/svgIcons/blocked2.svg"
     },
   ]
 
   return (
-    <div className="w-100 flex flex-row flex-wrap gap-5">
-      {stats.map((stat, index) => (
+    <div>
+      <div className="w-100 flex flex-row flex-wrap gap-5">
+        {stats.map((stat, index) => (
           <StatCard key={index} title={stat.title} stat={stat.stat} image={stat.image} />
-        )
-      )}
+          )
+          )}
+      </div>
     </div>
   )
 }
