829ef50f2c2368cbc16f84d25f7453944e51c308
saiksaif <116622210+saiksaif@users.noreply.github.com>
2023-11-03 08:27:00 +0500

    Fixed sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -21,47 +21,66 @@ import SubjectIcon from '@mui/icons-material/Subject';
 import SettingsIcon from '@mui/icons-material/Settings';
 import LogoutIcon from '@mui/icons-material/Logout';
 
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
+import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';
+import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
+import SpeakerOutlinedIcon from '@mui/icons-material/SpeakerOutlined';
+import FeedOutlinedIcon from '@mui/icons-material/FeedOutlined';
+import CameraOutlinedIcon from '@mui/icons-material/CameraOutlined';
+import LayersOutlinedIcon from '@mui/icons-material/LayersOutlined';
+import StoreOutlinedIcon from '@mui/icons-material/StoreOutlined';
+import SmartDisplayOutlinedIcon from '@mui/icons-material/SmartDisplayOutlined';
+import NewspaperOutlinedIcon from '@mui/icons-material/NewspaperOutlined';
+import BookOutlinedIcon from '@mui/icons-material/BookOutlined';
+import SummarizeOutlinedIcon from '@mui/icons-material/SummarizeOutlined';
+import CallOutlinedIcon from '@mui/icons-material/CallOutlined';
+import LiveHelpOutlinedIcon from '@mui/icons-material/LiveHelpOutlined';
+import NoteAddOutlinedIcon from '@mui/icons-material/NoteAddOutlined';
+
 import SidebarLink from './SidebarLink';
 
 function Sidebar() {
     return (
         <div className={"flex " + styles.sidebarMain}>
             <div className={"h-screen " + styles.sidebarInner}>
-                <div className="flex items-center justify-start mx-6 my-4">
-                    {/* <img className="h-10" src="/icons/rocket.svg" /> */}
+                <div className="flex items-center justify-start mx-6 mt-4 mb-0">
+                    <img className="h-10" src="/icon.png" />
                     <span className="text-gray-600 ml-4 text-2xl font-bold">
-                        StreamUp Dash
+                        StreamUp
                     </span>
                 </div>
-                <nav className={"mt-6 px-4 w-100 " + styles.sidebarNav}>
-                    <SidebarLink href="/" name="Dashboard" icon={<AppsIcon />} />
-                    <SidebarLink href="/users" name="Users" icon={<PeopleAltIcon />} />
-                    <SidebarLink href="/agencies" name="Agencies" icon={<AssignmentIcon />} />
-                    <SidebarLink href="/notifications" name="Notifications" icon={<NotificationsIcon />} />
+                <nav className={"mt-6 px-4 w-100 pt-1 " + styles.sidebarNav}>
+                    <SidebarLink href="/" name="Dashboard" icon={<GridViewOutlinedIcon />} />
+                    <SidebarLink href="/users" name="Users" icon={<PeopleAltOutlinedIcon />} />
+                    <SidebarLink href="/agencies" name="Agencies" icon={<AssignmentOutlinedIcon />} />
+                    <SidebarLink href="/notifications" name="Notifications" icon={<NotificationsActiveOutlinedIcon />} />
                     <SidebarLink href="/transactions" name="Transactions" icon={<AttachMoneyIcon />} />
-                    <SidebarLink href="/advertisements" name="Advertisements" icon={<SpeakerIcon />} />
-                    <SidebarLink href="/news-feed" name="Newsfeeds" icon={<DescriptionIcon />} />
-                    <SidebarLink href="/stories" name="Stories" icon={<CameraIcon />} />
-                    <SidebarLink href="/banners" name="Banners" icon={<LayersIcon />} />
-                    <SidebarLink href="/stores" name="Stores" icon={<LayersIcon />} />
+                    <SidebarLink href="/advertisements" name="Advertisements" icon={<SpeakerOutlinedIcon />} />
+                    <SidebarLink href="/news-feeds" name="Newsfeeds" icon={<FeedOutlinedIcon />} />
+                    <SidebarLink href="/stories" name="Stories" icon={<CameraOutlinedIcon />} />
+                    <SidebarLink href="/banners" name="Banners" icon={<LayersOutlinedIcon />} />
+                    <SidebarLink href="/stores" name="Stores" icon={<StoreOutlinedIcon />} />
                     <SidebarLink href="/audio-streaming" name="Audio Streaming" icon={<GraphicEqIcon />} />
-                    <SidebarLink href="/video-streaming" name="Video Streaming" icon={<GraphicEqIcon />} />
-                    <SidebarLink href="/news" name="News" icon={<ReportProblemIcon />} />
-                    <SidebarLink href="/blog" name="Blog" icon={<FeedIcon />} />
-                    <SidebarLink href="/subscribers" name="Subscribers" icon={<FeedIcon />} />
-                    <SidebarLink href="/contact" name="Contact" icon={<LocalPhoneIcon />} />
-                    <SidebarLink href="/faqs" name="FAQs" icon={<ErrorIcon />} />
-                    <SidebarLink href="/onboard" name="Onboard" icon={<NoteAddIcon />} />
+                    <SidebarLink href="/video-streaming" name="Video Streaming" icon={<SmartDisplayOutlinedIcon />} />
+                    <SidebarLink href="/news" name="News" icon={<NewspaperOutlinedIcon />} />
+                    <SidebarLink href="/blog" name="Blog" icon={<BookOutlinedIcon />} />
+                    <SidebarLink href="/subscribers" name="Subscribers" icon={<SummarizeOutlinedIcon />} />
+                    <SidebarLink href="/contact" name="Contact" icon={<CallOutlinedIcon />} />
+                    <SidebarLink href="/faqs" name="FAQs" icon={<LiveHelpOutlinedIcon />} />
+                    <SidebarLink href="/onboard" name="Onboard" icon={<NoteAddOutlinedIcon />} />
                     <SidebarLink href="/categories" name="Categories" icon={<SubjectIcon />} />
                     <SidebarLink href="/settings" name="Settings" icon={<SettingsIcon />} />
                 </nav>
                 <hr />
-                <div className={styles.sidebarLogout}>
-                    <Link className="w-100 text-gray-600 hover:text-gray-800 transition-colors duration-200 flex justify-center py-2 px-8" to="/login">
-                        <span className="mx-2 font-medium text-red-500">
+                <div className={styles.sidebarLogout + ' my-3'}>
+                    <Link 
+                    className="w-100 mx-4 text-gray-600 text-red-500 rounded-lg hover:text-white hover:bg-red-500 transition-colors duration-200 flex justify-center py-2 px-8" 
+                    to="/login">
+                        <span className="mx-2 font-medium">
                             Logout
                         </span>
-                        <LogoutIcon className='text-red-500' />
+                        <LogoutIcon className='' />
                     </Link>
                 </div>
             </div>
--- src/components/SidebarLink.jsx
+++ src/components/SidebarLink.jsx
@@ -2,18 +2,29 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useLocation } from "react-router-dom"
 
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+import SquareRoundedIcon from '@mui/icons-material/SquareRounded';
+import styles from "./styles/SidebarLink.module.css";
+
 function SidebarLink(props) {
 const location = useLocation();
 
   return (
     <Link 
-        className={"hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-1 transition-colors duration-200 text-gray-600 rounded-lg " + (props.href === location.pathname ? "bg-gray-100 text-gray-800" : "")} 
+        className={"flex items-center rounded-lg " + (props.href === location.pathname ? (styles.gradienticonA + ' text-white') : (styles.gradienticon + " hover:text-black text-black"))} 
         to={props.href}
     >
         {props.icon}
-        <span className="mx-2 font-normal">
+        <span className={"mx-2 font-normal "}>
             {props.name}
         </span>
+
+        {/* {props.href === location.pathname ? (
+            <div className={styles.gradienticonA}>
+                <SquareRoundedIcon />
+            </div>
+        ) : ""} */}
+        
         <span className="flex-grow text-right">
             {/* <button type="button" className="w-6 h-6 text-xs  rounded-full text-white bg-red-500">
                 <span className="p-1">
